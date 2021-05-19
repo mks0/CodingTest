4e4f49d88ad3656d7b88deb975d96032447a8cf7
mks0 <adelbera@naver.com>
2021-05-19 17:54:37 +0900

    Programmers-DFS_BFS - Word_conversion(Error correction)

--- Programmers/DFS_BFS/Word_conversion/Word_conversion.cpp
+++ Programmers/DFS_BFS/Word_conversion/Word_conversion.cpp
@@ -6,22 +6,21 @@ bool Compare(const string& str1, const string& str2) {
     int diff = 0;
     for (int i = 0; i < str1.size(); ++i)
         if (str1[i] != str2[i]) ++diff;
-    return (1 == diff) ? true : false;
+    return 1 == diff;
 }
 int solution(string begin, string target, vector<string> words) {
     struct QDat { vector<bool> visited; string to; int steps; };
     queue<QDat> q;
-    vector<bool> visited(words.size(), false);
-    q.push({visited, begin, 0});
+    q.push({vector<bool>(words.size(), false), begin, 0});
     while (!q.empty()) {
         auto top = q.front();
-        if (top.to == target)
-            break;
+        if (top.to == target) break;
         q.pop();
         for (int i = 0; i < words.size(); ++i) {
             if ((!top.visited[i]) && Compare(words[i], top.to)) {
                 top.visited[i] = true;
                 q.push({top.visited, words[i], top.steps + 1});
+				top.visited[i] = false;
             }
         }
     }
